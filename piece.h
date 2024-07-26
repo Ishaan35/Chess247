8b809bcb2984de1444fab76dc355aea869798282
ir3patel <ir3patel@uwaterloo.ca>
2024-07-25 21:02:05 -0400

    added pieceType enum

--- piece.h
+++ piece.h
@@ -1,13 +1,15 @@
 #include "movement.h"
 #include <memory>
+#include "pieceType.h"
 
 class Piece
 {
     Movement m;
+    PieceType type;
     int playerId;
 
 public:
-    Piece() {}
+    Piece(PieceType t, int pId) : type{t}, playerId{pId} {}
     virtual std::unique_ptr<Piece> clone() const = 0;
     virtual ~Piece() = 0;
 };
---
+++ pieceType.h
@@ -0,0 +1,14 @@
+#ifndef PIECETYPE_H
+#define PIECETYPE_H
+
+enum class PieceType
+{
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    King,
+    Queen
+};
+
+#endif
