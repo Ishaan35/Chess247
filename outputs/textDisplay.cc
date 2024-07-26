c20fa559e945d94270282c9b57ed1beacc25453b
ir3patel <ir3patel@uwaterloo.ca>
2024-07-26 18:56:05 -0400

    added player getter methods

--- outputs/textDisplay.cc
+++ outputs/textDisplay.cc
@@ -2,13 +2,40 @@
 
 TextDisplay::TextDisplay() {}
 
-void TextDisplay::renderToTerminal(std::vector<std::vector<std::unique_ptr<Piece>>> &board)
+char TextDisplay::convertPieceToChar(PieceType type, int playerId)
 {
+    if (type == PieceType::King)
+    {
+    }
+    return ' ';
+}
+
+void TextDisplay::renderToTerminal(const std::vector<std::vector<std::unique_ptr<Piece>>> &board)
+{
+    // highest row is rank 8
+    // lowest row is 1
+    // leftmost file is a
+    // rightmost file is h
+    for (size_t i = 0; i < board.size(); i++)
+    {
+        std::cout << board.size() - i << " ";
+        for (size_t j = 0; j < board[i].size(); j++)
+        {
+            if (board[i][j])
+            {
+                // std::cout << convertPieceToChar(board[i][j])
+            }
+        }
+    }
 }
 
 void TextDisplay::notify()
 {
-    // std::vector<std::vector<std::unique_ptr<Piece>>> &board =
+    if (auto chessState = subject.lock())
+    {
+        const std::vector<std::vector<std::unique_ptr<Piece>>> &board = chessState->getBoard();
+        renderToTerminal(board);
+    }
 }
 
 TextDisplay::~TextDisplay() {}
--- pieces/piece.cc
+++ pieces/piece.cc
@@ -4,3 +4,13 @@ std::unique_ptr<Piece> createPiece(PieceType t, int playerId)
 {
     return std::unique_ptr<Piece>();
 }
+
+int Piece::getPlayerId()
+{
+    return playerId;
+}
+
+PieceType Piece::getType()
+{
+    return type;
+}
