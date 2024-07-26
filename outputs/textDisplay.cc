5d8ef6745f21e0ff20608e5c2062462ce47701f5
ir3patel <ir3patel@uwaterloo.ca>
2024-07-26 18:16:49 -0400

    created subject and observer for text

---
+++ outputs/textDisplay.cc
@@ -0,0 +1,11 @@
+#include "textDisplay.h"
+
+TextDisplay::TextDisplay() {}
+
+void TextDisplay::renderToTerminal() {}
+
+void TextDisplay::notify()
+{
+}
+
+TextDisplay::~TextDisplay() {}
--- outputs/textDisplay.h
+++ outputs/textDisplay.h
@@ -1,3 +1,18 @@
-class TextDisplay{
+#ifndef GRAPHICSOBSERVER_H
+#define GRAPHICSOBSERVER_H
+#include "observer.h"
+#include <memory>
+#include <chessState.h>
 
+class TextDisplay : public Observer
+{
+    std::weak_ptr<ChessState> subject;
+
+public:
+    TextDisplay();
+    void renderToTerminal();
+    void notify() override;
+    ~TextDisplay();
 };
+
+#endif
