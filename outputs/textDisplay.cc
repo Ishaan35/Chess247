4d8e2fbca6c6e08cb17e8ccf8c7f8d41da54dffb
ir3patel <ir3patel@uwaterloo.ca>
2024-07-30 10:16:10 -0400

    fixed text display

--- outputs/textDisplay.cc
+++ outputs/textDisplay.cc
@@ -21,21 +21,23 @@ void TextDisplay::renderToTerminal(const std::vector<std::vector<std::unique_ptr
             }
             else
             {
-                std::cout << ' ' << std::endl;
+                std::cout << ' ';
             }
         }
         std::cout << std::endl;
     }
-    std::cout << std::endl
-              << std::endl;
+    std::cout << std::endl;
+    std::cout << ' ' << ' ';
     for (size_t i = 0; i < board[0].size(); i++)
-        std::cout << (char)('a' + i) << std::endl;
+        std::cout << (char)('a' + i);
+    std::cout << std::endl;
 }
 
 void TextDisplay::setSubject(std::shared_ptr<Subject> s, std::shared_ptr<Observer> o)
 {
-    // if we did not initialize subject yet (i.e if it was not done in the setup), attach it now. 
-    if(!subject.lock()){
+    // if we did not initialize subject yet (i.e if it was not done in the setup), attach it now.
+    if (!subject.lock())
+    {
         subject = std::dynamic_pointer_cast<ChessState>(s);
         if (auto subjectWkPtr = subject.lock())
         {
