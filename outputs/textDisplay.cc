7e770a8f25efbf21daade328cf188005c47f5d74
ir3patel <ir3patel@uwaterloo.ca>
2024-07-30 01:22:18 -0400

    fixed text display

--- outputs/textDisplay.cc
+++ outputs/textDisplay.cc
@@ -26,8 +26,8 @@ void TextDisplay::renderToTerminal(const std::vector<std::vector<std::unique_ptr
         }
         std::cout << std::endl;
     }
-    std::cout << std::endl
-              << std::endl;
+    std::cout << std::endl;
+    std::cout << "  ";
     for (size_t i = 0; i < board[0].size(); i++)
         std::cout << (char)('a' + i);
     std::cout << std::endl;
